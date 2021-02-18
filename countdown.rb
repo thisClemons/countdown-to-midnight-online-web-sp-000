4a1ed65e5b069a3ec2afbe15780d5bc8d36e13cd
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 01:31:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,15 @@
 def countdown(number)
   while number >0
     puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number >0
+    puts "#{number} SECOND(S)!"
     sleep(1)
     number -= 1
   end
